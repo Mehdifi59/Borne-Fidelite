f71211e3565902818b2b40ef89d76a39706309c7
Mehdi Zarouri <m.zarouri59@hotmail.fr>
2023-07-20 15:26:29 +0200

    ajout svg

--- app/number.js
+++ app/number.js
@@ -1,4 +1,5 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { COLORS } from "../constants";
 import NumberScreen from "../components/number/NumberScreen";
 import { Svg, Path } from "react-native-svg";
