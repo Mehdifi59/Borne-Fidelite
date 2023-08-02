b8713aa4920d1f53a476829ce0a3474131b2242c
Mehdi Zarouri <m.zarouri59@hotmail.fr>
2023-08-02 23:32:53 +0200

    ajout systeme derreur (pas fini)

--- front-end/components/commons/textInput/InputText.jsx
+++ front-end/components/commons/textInput/InputText.jsx
@@ -2,13 +2,28 @@ import { Kohana } from 'react-native-textinput-effects';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 import { View } from 'react-native';
 import { COLORS, FONTS } from '../../../constants';
+import { useState } from 'react';
+import styles from './TextInput.style';
 const InputText = ({label}) => {
+    const [text, setText] = useState('');
+    const [isRed, setIsRed] = useState(false);
+
+    const onChangeTextHandler = (newText) => {
+      setText(newText);
+      setIsRed(newText.includes('('));
+      console.log(newText);
+    };
 
     return (
       <View style={{height:80, marginTop:30}}>
         <Kohana
-        style={{ backgroundColor: '#f9f5ed',  width: "70%", elevation : 5, borderRadius:5 }}
+        style={[
+          styles.inputStyle,
+          isRed ? styles.alert : {borderColor : '#fff'},
+        ]}
         label={label}
+        onChangeText={onChangeTextHandler}
+        defaultValue={text}
         iconClass={FontAwesomeIcon}
         iconName={'user'}
         iconColor={'#FEBB35'}
---
+++ front-end/components/commons/textInput/TextInput.style.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+import { SIZES, COLORS, FONTS } from "../../../constants";
+
+const styles = StyleSheet.create({
+    
+    inputStyle: {
+        backgroundColor: '#f9f5ed',
+        width: "70%",
+        elevation : 5,
+        borderRadius:5,
+    },
+    
+    alert : {
+        borderRadius : 100,
+        backgroundColor: "#a56f",
+        borderColor : 'red',
+        borderWidth: 2
+    }
+
+})
+
+export default styles
