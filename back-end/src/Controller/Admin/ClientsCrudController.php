2ee8be52449ae3f21a3c31a4c7f3029994cb5d15
Mehdi Zarouri <m.zarouri59@hotmail.fr>
2023-08-19 10:31:56 +0200

    ajout admin

--- back-end/src/Controller/Admin/ClientsCrudController.php
+++ back-end/src/Controller/Admin/ClientsCrudController.php
@@ -4,6 +4,11 @@ namespace App\Controller\Admin;
 
 use App\Entity\Clients;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class ClientsCrudController extends AbstractCrudController
@@ -16,17 +21,24 @@ class ClientsCrudController extends AbstractCrudController
     public function configureCrud(Crud $crud): Crud
     {
         return $crud
-            ->setEntityLabelInPlural('Clients');
+
+            ->setEntityLabelInPlural('Clients')
+            ->setEntityLabelInSingular('Client')
+
+            ->setPageTitle('index', 'VazyGood - Gestion des Clients');
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnIndex(),
+            TextField::new('telephone'),
+            TextField::new('nom'),
+            TextField::new('prenom'),
+            IntegerField::new('points'),
+            DateTimeField::new('created_at')->setFormTypeOption('disabled', 'disabled'),
+
         ];
     }
-    */
 }
